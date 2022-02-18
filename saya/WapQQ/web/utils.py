5b2a43e8cc9b4e1b6bd549c01b99481be26fb9f1
CubeSugarCheese <cubesugarcheese@qq.com>
2022-02-18 23:36:38 +0800

    修复\n显示问题

--- saya/WapQQ/web/utils.py
+++ saya/WapQQ/web/utils.py
@@ -125,12 +125,12 @@ class ModifiedServer(Server):
 
 async def _build_env():
     loader = FileSystemLoader(current_path / "resources" / "templates")
-    environment = Environment(loader=loader, enable_async=True)
+    environment = Environment(loader=loader, enable_async=True, lstrip_blocks=True, trim_blocks=True)
     environment.filters["datefmt"] = lambda x: x.strftime('%Y-%m-%d')
     return environment
 
 
-async def render_template(file_name: str, **kwargs):
+async def render_template(file_name: str, **kwargs) -> str:
     environment = await _build_env()
     template = environment.get_template(file_name)
     rendered_template = await template.render_async(**kwargs)
